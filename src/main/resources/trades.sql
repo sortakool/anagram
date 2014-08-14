3023eafccc048fc178e0f78e4e02168436596977
Raymond Manaloto <ray.manaloto@gmail.com>
2014-08-13 22:43:04 -0500

    add comments

--- src/main/resources/trades.sql
+++ src/main/resources/trades.sql
@@ -1,4 +1,6 @@
+-----------------------------------------------------------------------------------------------------------------------------
 -- 1. Provide a query that shows the number of trades per client traded on the 1st of August, 2012.
+-----------------------------------------------------------------------------------------------------------------------------
 select tmp.CustomerId, tmp.CustomerName, CASE WHEN tmp.NumberOfTrades IS NULL THEN 0 ELSE tmp.NumberOfTrades END
 FROM
 (
@@ -16,8 +18,12 @@ FROM
 order by tmp.CustomerId
 ;
 
+-----------------------------------------------------------------------------------------------------------------------------
 -- 2. Provide a query that shows the trade details of the largest (= biggest quantity) trade done for each client.
 --   The result set should show the customer name, instrument name, quantity, price, and trade date.
+-----------------------------------------------------------------------------------------------------------------------------
+
+-- this version filters out customers without any trades
 select c.CustomerId, c.CustomerName, i.InstrumentName, t.Price, t.TradeDate, t.Quantity
 from
 CUSTOMERS c join Trades t on c.CustomerId = t.CustomerId
@@ -29,6 +35,7 @@ join
 order by c.CustomerName
 ;
 
+--this version shows all customers, but has nulls for instrumentname, price, tradedate and quantity for customers without any trades
 select c2.CustomerName, tmp2.instrumentname, tmp2.price, tmp2.tradedate, tmp2.quantity
 from Customers c2 left join
 (
@@ -44,8 +51,10 @@ from Customers c2 left join
 order by c2.CustomerName
 ;
 
+-----------------------------------------------------------------------------------------------------------------------------
 -- 3. Produce a query that shows the last date a client traded.
 --    The result set should show the customer id, customer name, and last trade date (or null if the client has never traded).
+-----------------------------------------------------------------------------------------------------------------------------
 select c.CustomerId, c.CustomerName, (select max(t2.TradeDate) from Trades t2 where c.CustomerId = t2.CustomerId) as LastTradeDate
 from CUSTOMERS c left join Trades t on c.CustomerId = t.CustomerId
 group by c.CustomerId, c.CustomerName
